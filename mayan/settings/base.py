2694cc450177fe4faea6062af860df3e8a1326e2
Urie <urie.sunli@gmail.com>
2021-12-08 13:10:17 +0800

    save

--- mayan/settings/base.py
+++ mayan/settings/base.py
@@ -9,8 +9,6 @@ from mayan.apps.smart_settings.utils import SettingNamespaceSingleton
 from .literals import DEFAULT_SECRET_KEY, SECRET_KEY_FILENAME, SYSTEM_DIR
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-DEBUG = False
-ALLOWED_HOSTS = ['*']
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -248,11 +246,9 @@ LANGUAGES = (
 
 SITE_ID = 1
 
-# STATIC_ROOT = os.environ.get(
-#     'MAYAN_STATIC_ROOT', os.path.join(MEDIA_ROOT, 'static')  # NOQA: F821
-# )
-
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.environ.get(
+    'MAYAN_STATIC_ROOT', os.path.join(MEDIA_ROOT, 'static')  # NOQA: F821
+)
 
 
 STATICFILES_FINDERS = (
